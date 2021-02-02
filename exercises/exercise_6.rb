a1fa9402b64c2aa025352c5b96a198631deafdf2
Rene Wang <rene.wang1@hotmail.com>
2021-02-02 04:59:50 +0000

    add validation

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -11,6 +11,15 @@ puts "----------"
 # Your code goes here ...
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, presence: true
+  validates :last_name , presence: true
+  validates :hourly_rate, numericality: {only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200}
+  validates :store, presence: true
+
+  def legal_pay?
+    (hourly_rate > 40 && hourly_rate < 200)
+  end
 end
 
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+puts Employee.count
