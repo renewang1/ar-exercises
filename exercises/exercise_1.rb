7bf66630fdce6a26a6505785b94d59eba1fa5723
Rene Wang <rene.wang1@hotmail.com>
2021-02-02 05:02:37 +0000

    add validation

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -6,6 +6,8 @@ puts "----------"
 # Your code goes below here ...
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 end
 
 Burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
